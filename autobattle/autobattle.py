f51c2be7ac92735a49c4ae5df0b2016e19a638c4
ZhendongWu <rsshinide38@163.com>
2021-05-09 14:14:02 +0800

    feat: 打轴功能补充日志及延时点击功能

--- autobattle/autobattle.py
+++ autobattle/autobattle.py
@@ -1,13 +1,10 @@
 import sys
-import os.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
 import collections
 import re
 import time
 import threading
-import yaml
 import numpy as np
+import yaml
 from pcrscript.ocr import Ocr
 from pcrscript.driver import Driver
 from pcrscript.constants import *
@@ -16,9 +13,9 @@ from typing import Iterable, Tuple
 from cv2 import cv2 as cv
 
 
-UB_ERROR_CHECK_DURATION = 2
+UB_ERROR_CHECK_DURATION = 3
 UB_THRESHOLD = 0.85
-INTERVAL = 0.5
+INTERVAL = 0.1
 LONG_INTERVAL = 1
 SP_REGIONS = (
     (677, 518, 763, 525),
@@ -35,10 +32,20 @@ UB_LOCATIONS = (
     (239, 446),
 )
 
+class Task:
+
+    def __init__(self, expecttime, loc, createtime):
+        self.expecttime = expecttime
+        self.createtime = createtime
+        self.loc = loc
+        self.triggertime = -1
+        self.delay = 0
+
 
 class AutoBattle:
 
     def __init__(self, driver: Driver):
+        print("初始化...")
         self._driver = driver
         self._ocr = Ocr(['en'], gpu=False, recog_network='english_g2')
         self._devicewidth, self._deviceheight = driver.getScreenSize()
@@ -53,14 +60,29 @@ class AutoBattle:
         self._pending = []
 
     def load(self, config_file):
+        print("加载配置文件中...")
         with open(config_file, encoding='utf-8') as f:
             self._config = yaml.load(f, Loader=yaml.FullLoader)
             self._operations.clear()
+            if 'ub_check_threshold' in self._config:
+                UB_THRESHOLD = float(self._config['ub_check_threshold'])
+            if 'ub_check_timeout' in self._config:
+                UB_ERROR_CHECK_DURATION = float(self._config['ub_check_timeout'])
+            if 'click_interval' in self._config:
+                INTERVAL = float(self._config['click_interval'])
+            if 'read_time_interval' in self._config:
+                LONG_INTERVAL = float(self._config['read_time_interval'])
             for operation in self._config['operation_list']:
-                seconds, location = operation.split()
-                self._operations[int(float(seconds))].append(int(location) - 1)
+                splits = operation.split()
+                seconds = int(float(splits[0]))
+                location = int(splits[1]) - 1
+                delay = 0
+                if len(splits) > 2:
+                    delay = float(splits[2])
+                self._operations[seconds].append((location, delay))
 
     def start(self):
+        print("准备启动脚本")
         if self._thread:
             self.stop()
         if self._config is None:
@@ -69,42 +91,53 @@ class AutoBattle:
         self._pause = False
         self._thread = threading.Thread(target=self._run)
         self._thread.start()
+        print("AutoBattle script run key commands:\n\033[1mp\033[0m  Pause.\n\033[1mr\033[0m  Resume.\n\033[1ms\033[0m  Stop.")
         self._waitinput()
 
     def pause(self):
+        print("执行暂停操作")
         self._pause = True
-        self._waitinput()
 
     def resume(self):
+        print("执行恢复操作")
         self._pause = False
         with self._condition:
             self._condition.notify()
-        self._waitinput()
 
     def stop(self):
+        print("执行停止操作")
         self._running = False
         with self._condition:
             self._condition.notify()
         if self._thread:
-            print("等待当前任务结束")
             self._thread.join()
             self._thread = None
 
-    def _waitinput(self):
-        opt = input('输入操作:').lower()
-        if opt == 's':
-            self.stop()
-        elif opt == 'r':
-            self.resume()
-        elif opt == 'p':
-            self.pause()
-        else:
-            print("未知操作")
-            self._waitinput()
+    def _waitinput(self):            
+        while True:
+            sys.stdout.flush()
+            opt = sys.stdin.readline().strip()
+            if opt == 's':
+                self.stop()
+                break
+            elif opt == 'r':
+                self.resume()
+            elif opt == 'p':
+                self.pause()
+            else:
+                if opt:
+                    print(f"收到未知操作符:{opt}")
+                else:
+                    print(f"收到EOF信息")
+        
 
     def _run(self):
         last_seconds,remain_seconds = None, None
+        last_readtime = 0
+        battle_start_time = 0
+        symbol_error_count = 0
         while self._running:
+            sys.stdout.flush()
             starttime = time.time()
             screenshot = self._driver.screenshot()
             if self._pause:
@@ -113,38 +146,71 @@ class AutoBattle:
                     self._condition.wait()
             else:
                 self._doresume(screenshot)
+            has_symbol = self._find_match_pos(screenshot, "btn_auto")
+            if not has_symbol:
+                symbol_error_count += 1
+                if symbol_error_count > 20:
+                    symbol_error_count = 0
+                    self._pending.clear()
+                    print("当前不处于战斗界面，已暂停")
+                    with self._condition:
+                        self._condition.wait()
+            elif battle_start_time == 0:
+                battle_start_time = starttime
+                print("已处于战斗界面，开始执行任务")
             self._errorcheck(screenshot)
             last_seconds = remain_seconds
-            remain_seconds = self._readtime(screenshot)
+            if not remain_seconds or starttime - last_readtime >= LONG_INTERVAL:
+                remain_seconds = self._readtime(screenshot)
+                last_readtime = time.time()
             if not remain_seconds:
                 self._waitnext(starttime)
                 continue
             sp = self._readsp(screenshot)
             if self._pending:
                 for i in range(len(self._pending) - 1, -1, -1):
-                    tseconds, loc, trigger = self._pending[i]
-                    if tseconds - remain_seconds > UB_ERROR_CHECK_DURATION:
+                    task = self._pending[i]
+                    if task.expecttime - remain_seconds > UB_ERROR_CHECK_DURATION:
+                        # 过期
                         self._pending.pop(i)
-                    elif trigger and sp[loc] < UB_THRESHOLD:
+                        self._logtask(task, isexpired=True)
+                    elif task.triggertime >= 0 and sp[task.loc] < UB_THRESHOLD:
+                        # 释放UB成功
                         self._pending.pop(i)
+                        self._logtask(task)
             if self._pending:
-                for tseconds, loc, trigger in self._pending:
-                    self._clickub(loc)
-            if self._pending:
-                print(self._pending)
+                for task in self._pending:
+                    clickable = True
+                    if task.triggertime < 0:
+                        if starttime - task.createtime - task.delay>= 0 and sp[task.loc] >= UB_THRESHOLD:
+                            task.triggertime = starttime
+                        elif task.delay > 0:
+                            clickable = False
+                    if clickable:
+                        self._clickub(task.loc)
+                        
             if last_seconds != remain_seconds:
                 newoperations = self._operations[remain_seconds]
                 if newoperations:
-                    for loc in newoperations:
-                        trigger = False
-                        if sp[loc] >= UB_THRESHOLD:
-                            trigger = True
+                    for loc, delay in newoperations:
+                        task = Task(remain_seconds, loc, starttime)
+                        task.delay = delay
+                        if delay == 0 and sp[loc] >= UB_THRESHOLD:
                             self._clickub(loc)
-                        else:
-                            trigger = False
-                        self._pending.append((remain_seconds, loc, trigger))
+                            task.triggertime = starttime
+                        self._pending.append(task)
 
             self._waitnext(starttime)
+
+    def _logtask(self,task, isexpired=False):
+        no = task.loc + 1
+        targettime = task.expecttime
+        offset = task.triggertime - task.createtime
+        if isexpired:
+            print(f"确认{no}UB释放超时，指定时间点为{targettime}，指定偏移{task.delay},触发真实时间偏移{offset}")
+        else:
+            print(f"确认{no}UB释放成功，指定时间点为{targettime}，指定偏移{task.delay},触发真实时间偏移{offset}")
+
 
     def _waitnext(self, starttime):
         consume = time.time() - starttime
@@ -248,10 +314,3 @@ class AutoBattle:
 
     def _roi(self, left, top, right, bottom) -> Tuple[int, int, int, int]:
         return (*self._pos(left, top), *self._pos(right, bottom))
-
-
-if __name__ == '__main__':
-    drivers = DNSimulator2("").get_dirvers()
-    auto_battle = AutoBattle(drivers[0])
-    auto_battle.load("autobattle_config.yml")
-    auto_battle.start()
