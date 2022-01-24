e4ac06c4efae56a5d516d60a22e446aff1fc228e
ZhendongWu <rsshinide38@163.com>
2022-01-25 04:04:32 +0800

    fix: 导包编写错误

--- plugins/battleline_extractor/battleline_extractor.py
+++ plugins/battleline_extractor/battleline_extractor.py
@@ -1,5 +1,5 @@
 from cv2 import cv2 as cv
-from battline_parser import *
+from battleline_parser import *
 import yaml
 
 if __name__ == "__main__":
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     result = parser.parse(cv.imread("test.png"))
     with open("output.yaml", "w", encoding="utf-8") as f:
         f.write("job_list:\n")
-        f.write(f"\t\"Test Output\":\n")
+        f.write(f"  \"Test Output\":\n")
         for line in result:
             if line.charactor == -1:
-                f.write(f"\t\t# {line.memo}\n")
+                f.write(f"    # {line.memo}\n")
             else:
-                f.write(f"\t\t- {line.time} {line.charactor} # {line.memo}\n")
+                f.write(f"    - {line.time} {line.charactor} # {line.memo}\n")
     
